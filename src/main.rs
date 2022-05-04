6bc0b163cf444653d8d30ba0f59c2d03c4f6bf1e
Ivan Achlaqullah <Ivan9aks2011@gmail.com>
2022-05-04 12:34:03 +0700

    Remove it from main.rs

--- src/main.rs
+++ src/main.rs
@@ -171,10 +171,7 @@ fn part_one(input: &[Option<_PassportId>], check_field: bool) -> u32 {
 }
 
 fn main() {
-    println!("{:?}", Day01::new(".\\input\\01.txt"));
     println!("{:?}", Day::new(1, ".\\input\\01.txt").unwrap());
-
-    println!("{:?}", Day02::new(".\\input\\02.txt"));
     println!("{:?}", Day::new(2, ".\\input\\02.txt").unwrap());
 
     println!("{:?}", Day03::new(".\\input\\03.txt"));
@@ -182,130 +179,6 @@ fn main() {
     let id_list = read_passport_id(".\\input\\04.txt");
     println!("Valid {}", part_one(&id_list, false));
     println!("Part 2: {}", part_one(&id_list, true));
-}
-
-#[derive(Debug)]
-struct Day01(i64, i64);
-
-impl Day01 {
-    fn new(filename: &str) -> Day01 {
-        let input = fs::read_to_string(filename).unwrap();
-        let input = {
-            let mut nums: HashSet<i64> = HashSet::new();
-            for i in input.lines() {
-                nums.insert(i.parse().unwrap());
-            }
-            nums
-        };
-
-        let r1 = {
-            let (x, y) = Day01::find_two(&input, 2020).unwrap();
-            x * y
-        };
-
-        let r2 = {
-            let (x, y, z) = Day01::find_three(&input, 2020).unwrap();
-            x * y * z
-        };
-
-        Day01(r1, r2)
-    }
-
-    fn find_two(inputs: &HashSet<i64>, max: i64) -> Option<(i64, i64)> {
-        for i in inputs.iter() {
-            let j = max - i;
-            if inputs.contains(&j) {
-                return Some((*i, j));
-            }
-        }
-        None
-    }
-
-    fn find_three(inputs: &HashSet<i64>, max: i64) -> Option<(i64, i64, i64)> {
-        for i in inputs.iter() {
-            let extra = max - i;
-            let extra = Day01::find_two(inputs, extra);
-            match extra {
-                None => continue,
-                Some((j, k)) => return Some((*i, j, k)),
-            }
-        }
-        None
-    }
-}
-
-#[derive(Debug)]
-struct _Pass {
-    min: u8,
-    max: u8,
-    target: char,
-    pass: String,
-}
-
-#[derive(Debug)]
-struct Day02(i64, i64);
-
-impl Day02 {
-    fn new(filename: &str) -> Day02 {
-        let input = Day02::_load(filename);
-        Day02(Day02::_part_one(&input), Day02::_part_two(&input))
-    }
-
-    fn _load(filename: &str) -> Vec<_Pass> {
-        let re = Regex::new(r"(?P<min>\d+).(?P<max>\d+).(?P<char>\w)..(?P<pass>\w+)").unwrap();
-        let input = fs::read_to_string(filename).unwrap();
-        let mut output: Vec<_Pass> = Vec::new();
-
-        for i in input.lines() {
-            let cap = re.captures(i);
-            let cap = match cap {
-                None => continue,
-                Some(x) => x,
-            };
-            let pass_char = cap["char"].chars().next().unwrap();
-            let result: _Pass = _Pass {
-                min: cap["min"].parse().unwrap(),
-                max: cap["max"].parse().unwrap(),
-                target: pass_char,
-                pass: cap["pass"].to_string(),
-            };
-            output.push(result);
-        }
-
-        output
-    }
-
-    fn _part_one(input: &[_Pass]) -> i64 {
-        let mut valid = 0;
-        for i in input.iter() {
-            let mut count = i.pass.clone();
-            count.retain(|x| x == i.target);
-            let count = count.len() as u8;
-            if i.min <= count && count <= i.max {
-                valid += 1;
-            };
-        }
-        valid
-    }
-
-    fn _part_two(input: &[_Pass]) -> i64 {
-        let mut valid = 0;
-        for i in input.iter() {
-            let pass_char: char = i.pass.chars().nth((i.min - 1).into()).unwrap();
-            let mut count = 0;
-            if pass_char == i.target {
-                count += 1;
-            }
-            let pass_char: char = i.pass.chars().nth((i.max - 1).into()).unwrap();
-            if pass_char == i.target {
-                count += 1;
-            }
-            if count == 1 {
-                valid += 1;
-            };
-        }
-        valid
-    }
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
